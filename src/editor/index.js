19825171eab6a9daab25f30e53f1e34959465351
Silviu Bogan <silviubogan@gmail.com>
2020-09-17 16:19:51 +0300

    To-be-refactored working prototype writing directly to form state context

--- src/editor/index.js
+++ src/editor/index.js
@@ -4,6 +4,11 @@ import { withFootnote } from './extensions';
 import { FOOTNOTE } from '../constants';
 import { FootnoteElement } from './render';
 import { defineMessages } from 'react-intl'; // , defineMessages
+import { v4 as uuid } from 'uuid';
+import {
+  getBlocksFieldname,
+  getBlocksLayoutFieldname,
+} from '@plone/volto/helpers';
 
 import './styles.less';
 
@@ -27,6 +32,64 @@ export default function install(config) {
     editSchema: FootnoteEditorSchema,
     extensions: [withFootnote],
     hasValue: (formData) => !!formData.footnote,
+    afterElementIsInserted: (editor, formContext, data) => {
+      console.log('afterElementIsInserted data is', data);
+      const fd = formContext.contextData.formData;
+      const bfn = getBlocksFieldname(fd);
+      const blfn = getBlocksLayoutFieldname(fd);
+
+      const blocks = fd[bfn];
+      const blocks_layout = fd[blfn];
+
+      // console.log('log', { blocks, blocks_layout });
+      let footnotesBlockExists = false;
+      for (const b in blocks) {
+        const bb = blocks[b];
+        if (bb['@type'] === 'slateFootnotes') {
+          footnotesBlockExists = true;
+          break;
+        }
+      }
+
+      if (!footnotesBlockExists) {
+        const nb = {
+          '@type': 'slateFootnotes',
+          title: 'Footnotes',
+        };
+        const nbWithId = {
+          '@type': 'slateFootnotes',
+          '@id': uuid(),
+          title: 'Footnotes',
+        };
+        const obj = {
+          formData: {
+            blocks: { ...blocks, [nbWithId['@id']]: nb },
+            blocks_layout: {
+              items: [...blocks_layout.items, nbWithId['@id']],
+            },
+          },
+        };
+        formContext.setContextData(obj);
+        // console.log('AFTER formContext:', formContext);
+      }
+
+      console.log(
+        'JUST LOGGING AROUND',
+        formContext /*editor.getBlockProps() */,
+      );
+      // function createSlateBlock(value, { index, onChangeBlock, onAddBlock }) {
+      //   return new Promise((resolve) => {
+      //     onAddBlock('slate', index + 1).then((id) => {
+      //       const options = {
+      //         '@type': 'slate',
+      //         value: JSON.parse(JSON.stringify(value)),
+      //         plaintext: serializeNodesToText(value),
+      //       };
+      //       onChangeBlock(id, options).then(() => resolve(id));
+      //     });
+      //   });
+      // }
+    },
     messages,
   };
   const [installFootnoteEditor] = makeInlineElementPlugin(opts);
